bb205c2343f9258dc91e56ed1eac19fcbdde5945
qwertytown4life <62229107+qwertytown4life@users.noreply.github.com>
2021-09-16 10:19:01 -0400

    Create 1.8.py

---
+++ 1.8.py
@@ -0,0 +1,20 @@
+import math
+
+def factors(number):
+    factsArray = []
+    for i in range(1, int(math.sqrt(number)) + 1, 1):
+        if number % i == 0:
+            factsArray.append(i)
+            factsArray.append(number // i)
+
+    factsArray.sort()
+    factsArray.pop()
+    return factsArray
+
+isPerfect = []
+N = int(input())
+
+if sum(factors(N)) == N:
+    print(f"{N} is perfect!")
+else:
+    print(f"{N} is not perfect.")
